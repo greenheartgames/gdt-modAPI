5c6eb5049c3bec3d9a4933ed6ed3a8e2a8d21877
Greenheart Games <support@greenheartgames.com>
2013-11-04 16:33:44 +1000

    fixed modAPI.js

--- modAPI.js
+++ modAPI.js
@@ -2,5 +2,20 @@
 	//this is the default modding API module that is loaded as the first mod and provides convenience methods for other mods.
 	//generally methods are added to the global object GDT.
 
-	requireLoad('./api/topics.js');
+	requireLoad(['mods/gdt-modAPI/api/topics.js'], ready);
+
+	var ready = function () {
+
+
+		//example
+		//GDT.addTopics([
+		//{
+		//	id: "Assassin",
+		//	name: "Assassin".localize("game topic"),
+		//	genreWeightings: [1, 1, 1, .6, .6, .6],
+		//	audienceWeightings: [0.6, 0.8, 1]
+		//}
+		//]);
+
+	};
 })();
