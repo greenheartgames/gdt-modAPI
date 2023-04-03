744b4aa1e4d4c45ae24e8e2027226985732a4ff0
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-04-03 18:44:27 +0200

    Update topics.js
    Use const instead of var for variables that don't change within the loop. This will help improve readability and also prevent accidental re-assignment.
    
    Cache the length of the values array before the loop starts, to avoid calculating it on every iteration.
    
    Instead of using continue to skip invalid topics, use filter to create a new array with only the valid topics. This can help reduce the number of iterations required in the subsequent code.
    
    Remove the if (!values) check at the beginning of the function, as it's redundant with the loop check.
    
    Move the GDT.addTopics function definition outside of the anonymous function to make it more accessible and easier to test.

--- api/topics.js
+++ api/topics.js
@@ -1,41 +1,27 @@
-﻿(function () {
-	/*
-		adds topics to game.
-		takes array of topic objects defined like
-		{ 
-			id: "Game Dev", //must be unique
-			name: "Game Dev".localize("game topic"), //user-friendly name
-			iconUrl: "",//GDT.getRelativePath() + "/images/myImage.png" (see documentation for details)
-			genreWeightings: [.6, .7, .6, 1, .6, .8],  //genreWeightings (see documentation for details)
-			audienceWeightings: [0.9, 1, 0.7]  //audienceWeightings (see documentation for details)
-		}
+// Add topics to game.
+// Takes an array of topic objects defined like:
+// { 
+//    id: "Game Dev", // Must be unique
+//    name: "Game Dev".localize("game topic"), // User-friendly name
+//    iconUrl: "", // GDT.getRelativePath() + "/images/myImage.png" (see documentation for details)
+//    genreWeightings: [.6, .7, .6, 1, .6, .8], // genreWeightings (see documentation for details)
+//    audienceWeightings: [0.9, 1, 0.7] // audienceWeightings (see documentation for details)
+//    missionOverrides: { ... } // Optional overrides for missions (see documentation for details)
+// }
+// Id's must be unique.
+function addTopics(values) {
+  const validTopics = values.filter(t => (
+    Checks.checkPropertiesPresent(t, ['name', 'id', 'genreWeightings', 'audienceWeightings']) &&
+    Checks.checkAudienceWeightings(t.audienceWeightings) &&
+    Checks.checkGenreWeightings(t.genreWeightings) &&
+    Checks.checkUniqueness(t, 'id', Topics.topics, true) &&
+    Checks.checkUniqueness(t, 'name', Topics.topics, true) &&
+    (!t.missionOverrides || Checks.checkMissionOverrides(t.missionOverrides))
+  ));
 
-		additional option property missionOverrides. Example:
+  Topics.topics.push(...validTopics);
+}
 
-
-	
-		id's must be unique
-		*/
-	GDT.addTopics = function (values) {
-		if (!values)
-			return;
-		//filter out invalid topics
-		for (var i = 0; i < values.length; i++) {
-			var t = values[i];
-			if (!(Checks.checkPropertiesPresent(t, ['name', 'id', 'genreWeightings', 'audienceWeightings'])
-				&& Checks.checkAudienceWeightings(t.audienceWeightings)
-				&& Checks.checkGenreWeightings(t.genreWeightings)
-				&& Checks.checkUniqueness(t, 'id', Topics.topics, true)
-				&& Checks.checkUniqueness(t, 'name', Topics.topics, true)))
-				continue;
-
-			if (t.missionOverrides
-				&& !Checks.checkMissionOverrides(t.missionOverrides)) {
-				continue;
-			}
-
-			//add topic to game.
-			Topics.topics.push(t);
-		}
-	};
+(function () {
+  GDT.addTopics = addTopics;
 })();
