e2913a5ebc2da31b90f833fbb058e50c3bd459b7
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-04-03 18:51:16 +0200

    Update events.js
    Instead of checking for the existence of event.notification or event.getNotification, you can just check if event.notification is a function using typeof event.notification === 'function'.
    
    You can also use Checks.checkPropertiesPresent to check for the existence of event.notification or event.getNotification instead of using if statements.
    
    It might be useful to include an error message if any of the checks fail.

--- api/events.js
+++ api/events.js
@@ -1,10 +1,14 @@
-﻿(function () {
+(function () {
 	GDT.addEvent = function (event) {
-		if (!Checks.checkPropertiesPresent(event, ['id'])
-			&& (event.notification || event.getNotification))
+		if (!Checks.checkPropertiesPresent(event, ['id', 'notification']) 
+			&& typeof event.notification !== 'function') {
+			Checks.error('Invalid event object: ' + JSON.stringify(event));
 			return;
-		if (!Checks.checkUniqueness(event, 'id', DecisionNotifications.modNotifications))
+		}
+		if (!Checks.checkUniqueness(event, 'id', DecisionNotifications.modNotifications)) {
+			Checks.error('Duplicate event ID: ' + event.id);
 			return;
+		}
 
 		DecisionNotifications.modNotifications.push(event);
 	}
