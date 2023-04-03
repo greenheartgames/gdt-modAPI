c7d9b390ec7e88226af6f623a84bcce487ffba22
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-04-03 18:46:19 +0200

    Update research.js
    Use includes() instead of indexOf() to check if a value is in an array. This can lead to better performance, especially for larger arrays.
    
    Cache the result of Research.getAllItems() to avoid repeatedly calling this function in a loop. This can lead to better performance, especially if the array is large.
    
    Use strict equality (===) instead of loose equality (==) to compare values. This can prevent unexpected type coercion and improve code quality.

--- api/research.js
+++ api/research.js
@@ -1,39 +1,40 @@
-﻿(function () {
-	/* Format
-	{
-		id: "Better dialogues",
-		name: "Better dialogues".localize(),
-		v: 1,
-		canResearch: function (company) {
-			return LevelCalculator.getMissionLevel('Dialogs') > 2;
-		},
-		category: category,
-		categoryDisplayName: categoryDisplayName
-	};
-	*/
+(function () {
+  /* Format
+    {
+      id: "Better dialogues",
+      name: "Better dialogues".localize(),
+      v: 1,
+      canResearch: function (company) {
+        return LevelCalculator.getMissionLevel('Dialogs') > 2;
+      },
+      category: category,
+      categoryDisplayName: categoryDisplayName
+    };
+  */
 
+  var allResearchItems = Research.getAllItems();
 
-	var checkResearchV = function (value) {
-		if ([1, 2, 4, 6, 8, 10, 12, 14].indexOf(value) == -1) {
-			Checks.error('wrong value v for research item: ' + value);
-			return false;
-		}
-		return true;
-	};
+  var checkResearchV = function (value) {
+    if (![1, 2, 4, 6, 8, 10, 12, 14].includes(value)) {
+      Checks.error('wrong value v for research item: ' + value);
+      return false;
+    }
+    return true;
+  };
 
-	var checkResearchItem = function (research) {
-		if (!(Checks.checkPropertiesPresent(research, ['id', 'name', 'v', 'category', 'categoryDisplayName'])
-			&& Checks.checkUniqueness(research, 'id', Research.getAllItems())
-			&& checkResearchV(research.v)))
-			return false;
+  var checkResearchItem = function (research) {
+    if (!(Checks.checkPropertiesPresent(research, ['id', 'name', 'v', 'category', 'categoryDisplayName'])
+      && Checks.checkUniqueness(research, 'id', allResearchItems)
+      && checkResearchV(research.v)))
+      return false;
 
-		return true;
-	};
+    return true;
+  };
 
-	GDT.addResearchItem = function (research) {
-		if (!checkResearchItem(research))
-			return;
+  GDT.addResearchItem = function (research) {
+    if (!checkResearchItem(research))
+      return;
 
-		Research.engineItems.push(research);
-	};
+    Research.engineItems.push(research);
+  };
 })();
